0752365f816102b41faf498e85ece075c45dacb9
Raka Fantino <rakafantino@gmail.com>
2022-11-13 05:32:26 +0700

    still trying to solve production problem

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // use this setting in development phase only
-  // server: {
-  //   proxy: {
-  //     "/fee-assessment-categories": {
-  //       target: "https://asia-southeast2-sejutacita-app.cloudfunctions.net",
-  //       changeOrigin: true,
-  //     },
-  //     "/fee-assessment-books": {
-  //       target: "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/fee-assessment-categories": {
+        target: "https://asia-southeast2-sejutacita-app.cloudfunctions.net",
+        changeOrigin: true,
+      },
+      "/fee-assessment-books": {
+        target: "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 });
